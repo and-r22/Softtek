a3b21a560057be7a9081577913838f5bc54760d9
Mustafa <78199096+Mustafa-Elout@users.noreply.github.com>
2022-06-09 12:49:50 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
 import { Not404Component } from './pages/not404/not404.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path : 'login', component: LoginComponent},
+  { path: 'registrar', component: RegistroComponent },
   {path: 'pages',
     component: LayoutComponent,
     loadChildren: () =>
     import('./pages/pages.module').then(m => m.PagesModule)
   },
   {path: 'not-404', component: Not404Component},
-  
+
   { path:'**', redirectTo:'not-404'}
 ];
 
